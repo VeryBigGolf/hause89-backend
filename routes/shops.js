0b541b2bc810e7e2962cd9e7669c77418ca811b2
Natthaphat Changngeon <nnnatthaphat69@gmail.com>
2026-03-01 01:35:38 +0700

    updated routes

---
+++ routes/shops.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+const { protect, authorize } = require("../middleware/auth");
+
+const {
+  getShop,
+  getShops,
+  createShop,
+  updateShop,
+  deleteShop,
+} = require("../controllers/shops");
+
+const appointmentRouter = require("./appointments");
+router.use("/:shopId/appointments", appointmentRouter);
+
+router.route("/").get(getShops).post(protect, authorize("admin"), createShop);
+router
+  .route("/:id")
+  .get(getShop)
+  .put(protect, authorize("admin"), updateShop)
+  .delete(protect, authorize("admin"), deleteShop);
+
+module.exports = router;
