42ac0b4de8e42df3d4f5c84978fcee15b3242b27
popxoplop <kiatthanapat.p@gmail.com>
2026-03-01 02:15:41 +0700

    fix register in controllers

--- controllers/auth.js
+++ controllers/auth.js
@@ -25,7 +25,7 @@ const sendTokenResponse = (user, statusCode, res) => {
 //@access   Public
 exports.register = async (req, res, next) => {
   try {
-    const { name, email, password, role } = req.body;
+    const { name, email, password, role, tel } = req.body;
     const user = await User.create({
       name,
       email,
